e00c636bad77db9d3ebf19ca764457dc20de7831
Kiranm3108 <kiranmeesala065@gmail.com>
2024-10-05 11:28:42 +0530

    Add JavaScript files and HTML file

---
+++ display.js
@@ -0,0 +1,5 @@
+const displayInfo=()=>//function definition
+{
+    console.log("Hi this is display function");
+}
+displayInfo();//function calling
---
+++ text.js
@@ -0,0 +1,7 @@
+let collegeName=new String("gnit college");
+console.log(collegeName.startsWith("gnit"));
+console.log(collegeName.startsWith("Gnit"));
+console.log(collegeName.startsWith("college"));
+console.log(collegeName.endsWith("gnit"));
+console.log(collegeName.endsWith("Gnit"));
+console.log(collegeName.endsWith("college"));
